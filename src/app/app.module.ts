0ffb3c985da6de2e524384f63e9180852fb7bf5a
Mognia <1mognia@gmail.com>
2022-06-27 14:02:54 +0430

    initialize app basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './components/head/head.component';
+import { DaysHolderComponent } from './components/days-holder/days-holder.component';
+import { LabelsSectionComponent } from './components/labels-section/labels-section.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    DaysHolderComponent,
+    LabelsSectionComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
