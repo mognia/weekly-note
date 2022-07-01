6060407854561f3ad03c7a8fea02fbd7bcf460e1
mognia <1mognia@gmail.com>
2022-07-01 19:42:41 +0430

    adding labels title

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -7,10 +7,17 @@ import {Note} from "../../app.component";
 })
 export class CardsComponent implements OnInit {
 @Input() note: Note | undefined;
-
+  duration : number=1;
   constructor() { }
 
   ngOnInit(): void {
+    this.calculateDuration()
   }
-
+  calculateDuration(){
+    let endDay = typeof this.note?.endDate==='string' ? parseInt(this.note?.endDate.split('/')[0]) : 0;
+    let startDay = typeof this.note?.startDate==='string' ? parseInt(this.note?.startDate.split('/')[0]):1;
+    if (endDay> startDay) {
+      this.duration = endDay - startDay
+    }
+  }
 }
