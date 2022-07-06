719a223d3a48800fa75944a16098ab8ff03dc74a
mognia <1mognia@gmail.com>
2022-07-06 16:52:55 +0430

    writing tests for calender.component

--- src/app/components/calender/calender.component.spec.ts
+++ src/app/components/calender/calender.component.spec.ts
@@ -1,25 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalenderComponent } from './calender.component';
-
+import {click, expectText} from "../../helpers/spec-helpers/element.spec-helper";
+const startCount = 1
 describe('CalenderComponent', () => {
   let component: CalenderComponent;
   let fixture: ComponentFixture<CalenderComponent>;
 
+  function expectCount(count: number): void {
+    expectText(fixture, 'count', String(count));
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CalenderComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+    }).compileComponents();
     fixture = TestBed.createComponent(CalenderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('shows the start week', () => {
+    expectCount(startCount);
+  });
+  it('increments the week', () => {
+    click(fixture, 'increment-button');
+    fixture.detectChanges();
+    expectCount(startCount + 1);
+  });
+
+  it('decrements the week', () => {
+    click(fixture, 'decrement-button');
+    fixture.detectChanges();
+    expectCount(startCount - 1);
   });
 });
